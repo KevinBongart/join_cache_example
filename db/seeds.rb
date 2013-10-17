55d3c1a822a856e1979d4f25343b9da7636067c4
Kevin Bongart <contact@kevinbongart.net>
2013-10-17 15:38:32 -0400

    Better database seeds, better speed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,36 @@
+LIMIT = 1000
+
+def rand_time(from, to=Time.now)
+  Time.at(rand_in_range(from.to_f, to.to_f))
+end
+
+def rand_in_range(from, to)
+  rand * (to - from) + from
+end
+
 ActiveRecord::Base.transaction do
-  1000.times do |i|
+  LIMIT.times do |i|
     Assembly.create(name: "Assembly #{i}")
-    Part.create(part_number: "#{i}")
+    Part.create(part_number: i)
     Physician.create(name: "Physician #{i}")
     Patient.create(name: "Patient #{i}")
   end
 end
 
-Part.find_each do |part|
-  part.assemblies = Assembly.all.sample(rand(100..1000))
+ActiveRecord::Base.transaction do
+  (LIMIT * 100).times do |i|
+    AssembliesParts.create(
+      assembly_id: rand(1..LIMIT),
+      part_id:   rand(1..LIMIT)
+    )
+  end
 end
 
-Patient.find_each do |patient|
-  patient.physicians = Physician.all.sample(rand(100..1000))
+ActiveRecord::Base.transaction do
+  (LIMIT * 100).times do |i|
+    Appointment.create(
+      physician_id: rand(1..LIMIT),
+      patient_id:   rand(1..LIMIT)
+    )
+  end
 end
--- lib/tasks/speed.rake
+++ lib/tasks/speed.rake
@@ -1,11 +1,32 @@
-task join: :environment do
+task has_and_belongs_to_many: :environment do
   Assembly.find_each do |assembly|
-    p assembly.parts.order(:part_number)
+    assembly.parts.map
   end
 end
 
-task join_cache: :environment do
+task has_and_belongs_to_many_with_join_cache: :environment do
   Assembly.find_each do |assembly|
-    p assembly.cached_parts.order(:part_number)
+    assembly.cached_parts.map
   end
+end
+
+task has_many_through: :environment do
+  Physician.find_each do |physician|
+    physician.patients.map
+  end
+end
+
+task has_many_through_with_join_cache: :environment do
+  Physician.find_each do |physician|
+    physician.cached_patients.map
+  end
+end
+
+task speed_test: :environment do
+  require 'rake_performance'
+
+  Rake::Task["has_and_belongs_to_many"].invoke
+  Rake::Task["has_and_belongs_to_many_with_join_cache"].invoke
+  Rake::Task["has_many_through"].invoke
+  Rake::Task["has_many_through_with_join_cache"].invoke
 end
